75d9a7b4be718c81f95fc72025b104631ccdb146
wuliuqii <34090258+wuliuqii@users.noreply.github.com>
2024-11-14 22:52:23 +0800

    Initial from https://github.com/Smithay/client-toolkit/blob/master/examples/simple_layer.rs

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = eachSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell (
+            with pkgs;
+            rec {
+              packages = [ ];
+
+              nativeBuildInputs = [
+                pkg-config
+                mold
+                clang
+              ];
+
+              buildInputs = [
+                udev
+                # alsa-lib
+                vulkan-loader
+                libxkbcommon
+                pixman
+                seatd
+                libinput
+                wayland
+                fontconfig
+              ];
+
+              LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+            }
+          );
+        }
+      );
+    };
+}
